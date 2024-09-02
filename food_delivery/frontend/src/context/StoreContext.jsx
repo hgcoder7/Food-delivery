ac1e7b4f09530622d6968d7ccec60a20a45a5277
Harshit Gupta <119857118+hgcoder7@users.noreply.github.com>
2024-09-02 17:46:18 +0530

    Update StoreContext.jsx

--- food_delivery/frontend/src/context/StoreContext.jsx
+++ food_delivery/frontend/src/context/StoreContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
 import axios from "axios";
 
 export const StoreContext=createContext(null);
